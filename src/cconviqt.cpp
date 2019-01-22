51c0e8ae1d209043abedf8b6cdd048cfdf10ecac
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2019-01-21 21:28:36 -0800

    When reading the beam or the sky with user-set expansion orders, insufficient lmax and mmax are fatal errors

--- src/cconviqt.cpp
+++ src/cconviqt.cpp
@@ -8,284 +8,303 @@
 extern "C" {
 
 #include <string.h>
-  
-  void *conviqt_beam_new() { return new(std::nothrow) conviqt::beam; }
-  
-  int conviqt_beam_del( void *ptr ) {
-    try {
-      conviqt::beam *ref = reinterpret_cast< conviqt::beam * >( ptr );
-      delete ref;
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
-  
-  int conviqt_beam_read( void *ptr, long beamlmax, long beammmax, char pol, char *infile_beam, MPI_Comm comm ) {
-    try {
-      conviqt::beam *ref = reinterpret_cast< conviqt::beam * >( ptr );
-      ref->read( beamlmax, beammmax, pol, infile_beam, comm );
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
 
-  int conviqt_beam_lmax( void *ptr ) {
-    try {
-      conviqt::beam *ref = reinterpret_cast< conviqt::beam * >( ptr );
-      return ref->get_lmax();
-    } catch (...) {
-      return -1;
-    }
-  }
+    void *conviqt_beam_new() { return new(std::nothrow) conviqt::beam; }
 
-  int conviqt_beam_mmax( void *ptr ) {
-    try {
-      conviqt::beam *ref = reinterpret_cast< conviqt::beam * >( ptr );
-      return ref->get_mmax();
-    } catch (...) {
-      return -1;
+    int conviqt_beam_del(void *ptr) {
+        try {
+            conviqt::beam *ref = reinterpret_cast< conviqt::beam * >(ptr);
+            delete ref;
+        } catch (...) {
+            return -1;
+        }
+        return 0;
     }
-  }
 
-  void *conviqt_sky_new() { return new(std::nothrow) conviqt::sky; }
-
-  int conviqt_sky_del( void *ptr ) {
-    try {
-      conviqt::sky *ref = reinterpret_cast< conviqt::sky * >( ptr );
-      delete ref;
-    } catch (...) {
-      return -1;
+    int conviqt_beam_read(void *ptr, long beamlmax, long beammmax, char pol,
+                          char *infile_beam, MPI_Comm comm) {
+        try {
+            conviqt::beam *ref = reinterpret_cast< conviqt::beam * >(ptr);
+            ref->read(beamlmax, beammmax, pol, infile_beam, comm);
+        } catch (std::exception &e) {
+            std::cerr << "conviqt_beam_read threw an exception: " << e.what() << std::endl;
+            return -1;
+        } catch (...) {
+            std::cerr << "conviqt_beam_read threw an exception." << std::endl;
+            return -1;
+        }
+        return 0;
     }
-    return 0;
-  }
-  
-  int conviqt_sky_read( void *ptr, long skylmax, char pol, char *infile_sky, double fwhm_deconv_sky, MPI_Comm comm ) {
-    try {
-      conviqt::sky *ref = reinterpret_cast< conviqt::sky * >( ptr );
-      ref->read( skylmax, pol, infile_sky, fwhm_deconv_sky, comm );
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
-  
-  int conviqt_sky_lmax( void *ptr ) {
-    try {
-      conviqt::sky *ref = reinterpret_cast< conviqt::sky * >( ptr );
-      return ref->get_lmax();
-    } catch (...) {
-      return -1;
-    }
-  }
 
-  int conviqt_sky_remove_monopole( void *ptr ) {
-    try {
-      conviqt::sky *ref = reinterpret_cast< conviqt::sky * >( ptr );
-      ref->remove_monopole();
-    } catch (...) {
-      return -1;
+    int conviqt_beam_lmax(void *ptr) {
+        try {
+            conviqt::beam *ref = reinterpret_cast< conviqt::beam * >(ptr);
+            return ref->get_lmax();
+        } catch (...) {
+            return -1;
+        }
     }
-    return 0;
-  }
 
-  int conviqt_sky_remove_dipole( void *ptr ) {
-    try {
-      conviqt::sky *ref = reinterpret_cast< conviqt::sky * >( ptr );
-      ref->remove_dipole();
-    } catch (...) {
-      return -1;
+    int conviqt_beam_mmax(void *ptr) {
+        try {
+            conviqt::beam *ref = reinterpret_cast< conviqt::beam * >(ptr);
+            return ref->get_mmax();
+        } catch (...) {
+            return -1;
+        }
     }
-    return 0;
-  }
 
-  void *conviqt_detector_new() { return new(std::nothrow) conviqt::detector; }
+    void *conviqt_sky_new() { return new(std::nothrow) conviqt::sky; }
 
-  void *conviqt_detector_new_with_id( char *det_id ) { return new(std::nothrow) conviqt::detector(det_id); }
-
-  int conviqt_detector_del( void *ptr ) {
-    try {
-      conviqt::detector *ref = reinterpret_cast< conviqt::detector * >( ptr );
-      delete ref;
-    } catch (...) {
-      return -1;
+    int conviqt_sky_del(void *ptr) {
+        try {
+            conviqt::sky *ref = reinterpret_cast< conviqt::sky * >(ptr);
+            delete ref;
+        } catch (...) {
+            return -1;
+        }
+        return 0;
     }
-    return 0;
-  }
-  
-  int conviqt_detector_set_epsilon( void *ptr, double epsilon ) {
-    try {
-      conviqt::detector *ref = reinterpret_cast< conviqt::detector * >( ptr );
-      ref->set_epsilon( epsilon );
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
 
-  int conviqt_detector_get_epsilon( void *ptr, double *epsilon ) {
-    try {
-      conviqt::detector *ref = reinterpret_cast< conviqt::detector * >( ptr );
-      *epsilon = ref->get_epsilon();
-    } catch (...) {
-      return -1;
+    int conviqt_sky_read(void *ptr, long skylmax, char pol, char *infile_sky,
+                         double fwhm_deconv_sky, MPI_Comm comm) {
+        try {
+            conviqt::sky *ref = reinterpret_cast< conviqt::sky * >(ptr);
+            ref->read(skylmax, pol, infile_sky, fwhm_deconv_sky, comm);
+        } catch (std::exception &e) {
+            std::cerr << "conviqt_sky_read threw an exception: " << e.what() << std::endl;
+            return -1;
+        } catch (...) {
+            std::cerr << "conviqt_sky_read threw an exception." << std::endl;
+            return -1;
+        }
+        return 0;
     }
-    return 0;
-  }
 
-  int conviqt_detector_set_id( void *ptr, char *det_id ) {
-    try {
-      conviqt::detector *ref = reinterpret_cast< conviqt::detector * >( ptr );
-      ref->set_id( det_id );
-    } catch (...) {
-      return -1;
+    int conviqt_sky_lmax(void *ptr) {
+        try {
+            conviqt::sky *ref = reinterpret_cast< conviqt::sky * >(ptr);
+            return ref->get_lmax();
+        } catch (...) {
+            return -1;
+        }
     }
-    return 0;
-  }
 
-  int conviqt_detector_get_id( void *ptr, char *det_id ) {
-    try {
-      conviqt::detector *ref = reinterpret_cast< conviqt::detector * >( ptr );
-      strcpy( det_id, ref->get_id().c_str() );
-    } catch (...) {
-      return -1;
+    int conviqt_sky_remove_monopole(void *ptr) {
+        try {
+            conviqt::sky *ref = reinterpret_cast< conviqt::sky * >(ptr);
+            ref->remove_monopole();
+        } catch (...) {
+            return -1;
+        }
+        return 0;
     }
-    return 0;
-  }
 
-  void *conviqt_pointing_new() { return new(std::nothrow) conviqt::pointing; }
-
-  int conviqt_pointing_del( void *ptr ) {
-    try {
-      conviqt::pointing *ref = reinterpret_cast< conviqt::pointing * >( ptr );
-      delete ref;
-    } catch (...) {
-      return -1;
+    int conviqt_sky_remove_dipole(void *ptr) {
+        try {
+            conviqt::sky *ref = reinterpret_cast< conviqt::sky * >(ptr);
+            ref->remove_dipole();
+        } catch (...) {
+            return -1;
+        }
+        return 0;
     }
-    return 0;
-  }
-  
-  int conviqt_pointing_alloc( void *ptr, long n ) {
+
+    void *conviqt_detector_new() { return new(std::nothrow) conviqt::detector; }
+
+    void *conviqt_detector_new_with_id(char *det_id) {
+        return new(std::nothrow) conviqt::detector(det_id);
+    }
+
+    int conviqt_detector_del(void *ptr) {
+        try {
+            conviqt::detector *ref = reinterpret_cast< conviqt::detector * >(ptr);
+            delete ref;
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_detector_set_epsilon(void *ptr, double epsilon) {
+        try {
+            conviqt::detector *ref = reinterpret_cast< conviqt::detector * >(ptr);
+            ref->set_epsilon(epsilon);
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_detector_get_epsilon(void *ptr, double *epsilon) {
+        try {
+            conviqt::detector *ref = reinterpret_cast< conviqt::detector * >(ptr);
+            *epsilon = ref->get_epsilon();
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_detector_set_id(void *ptr, char *det_id) {
+        try {
+            conviqt::detector *ref = reinterpret_cast< conviqt::detector * >(ptr);
+            ref->set_id(det_id);
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_detector_get_id(void *ptr, char *det_id) {
+        try {
+            conviqt::detector *ref = reinterpret_cast< conviqt::detector * >(ptr);
+            strcpy(det_id, ref->get_id().c_str());
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    void *conviqt_pointing_new() { return new(std::nothrow) conviqt::pointing; }
+
+    int conviqt_pointing_del(void *ptr) {
+        try {
+            conviqt::pointing *ref = reinterpret_cast< conviqt::pointing * >(ptr);
+            delete ref;
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_pointing_alloc(void *ptr, long n) {
 #ifdef DEBUG
-    std::cout << "Allocating " << n << " doubles for pointing" << std::endl;
+        std::cout << "Allocating " << n << " doubles for pointing" << std::endl;
 #endif
-    try {
-      conviqt::pointing *ref = reinterpret_cast< conviqt::pointing * >( ptr );
-      ref->alloc( n );
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
-
-  double *conviqt_pointing_data( void *ptr ) {
-    try {
-      conviqt::pointing *ref = reinterpret_cast< conviqt::pointing * >( ptr );
-      return ref->begin();
-    } catch (...) {
-      return NULL;
-    }
-  }
-
-  void *conviqt_convolver_new( void *skyptr, void *beamptr, void *detptr, char pol, long lmax, long beammmax, long order, MPI_Comm comm ) {
-
-#ifdef DEBUG
-    int size, rank, err;
-    
-    err = MPI_Comm_size( comm, &size );
-    if ( err ) {
-      std::cout << "conviqt_convolver_new: Failed to query communicator size." << std::endl;
-      return NULL;
+        try {
+            conviqt::pointing *ref = reinterpret_cast< conviqt::pointing * >(ptr);
+            ref->alloc(n);
+        } catch (...) {
+            return -1;
+        }
+        return 0;
     }
 
-    err = MPI_Comm_rank( comm, &rank );
-    if ( err ) {
-      std::cout << "conviqt_convolver_new: Failed to query task rank." << std::endl;
-      return NULL;
+    double *conviqt_pointing_data(void *ptr) {
+        try {
+            conviqt::pointing *ref = reinterpret_cast< conviqt::pointing * >(ptr);
+            return ref->begin();
+        } catch (...) {
+            return NULL;
+        }
     }
 
-    std::cout << "conviqt_convolver_new: Task " << rank << " / " << size << " initializing." << std::endl;
-
-    std::cout << "conviqt_convolver_new called with pol = " << int(pol) << ", lmax = " << lmax << ", beammmax = " << beammmax << ", order = " << order << std::endl;
-#endif
-
-    if ( lmax > LMAXMAX || beammmax > LMAXMAX ) {
-      std::cerr << "Suspiciously large convolver parameters: lmax = " << lmax << ", beammax = " << beammmax << std::endl;
-      return NULL;
-    }
-
-    conviqt::sky *skyref = reinterpret_cast< conviqt::sky * >( skyptr );
-    conviqt::beam *beamref = reinterpret_cast< conviqt::beam * >( beamptr );
-    conviqt::detector *detref = reinterpret_cast< conviqt::detector * >( detptr );
-
-    return new(std::nothrow) conviqt::convolver( skyref, beamref, detref, pol, lmax, beammmax, order, comm );
-  }
-
-  int conviqt_convolver_del( void *ptr ) {
-    try {
-      conviqt::convolver *ref = reinterpret_cast< conviqt::convolver * >( ptr );
-      delete ref;
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
-  
-  int conviqt_convolver_set_sky( void *cnvptr, void *skyptr ) {
-    try {
-      conviqt::convolver *cnvref = reinterpret_cast< conviqt::convolver * >( cnvptr );
-      conviqt::sky *skyref = reinterpret_cast< conviqt::sky * >( skyptr );
-      cnvref->set_sky( skyref );
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
-
-  int conviqt_convolver_set_beam( void *cnvptr, void *beamptr ) {
-    try {
-      conviqt::convolver *cnvref = reinterpret_cast< conviqt::convolver * >( cnvptr );
-      conviqt::beam *beamref = reinterpret_cast< conviqt::beam * >( beamptr );
-      cnvref->set_beam( beamref );
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
-
-  int conviqt_convolver_set_detector( void *cnvptr, void *detptr ) {
-    try {
-      conviqt::convolver *cnvref = reinterpret_cast< conviqt::convolver * >( cnvptr );
-      conviqt::detector *detref = reinterpret_cast< conviqt::detector * >( detptr );
-      cnvref->set_detector( detref );
-    } catch (...) {
-      return -1;
-    }
-    return 0;
-  }
-
-  int conviqt_convolver_convolve( void *cnvptr, void *pntptr, char calibrate ) {
-    try {
-      conviqt::convolver *cnvref = reinterpret_cast< conviqt::convolver * >( cnvptr );
-      conviqt::pointing *pntref = reinterpret_cast< conviqt::pointing * >( pntptr );
+    void *conviqt_convolver_new(void *skyptr, void *beamptr, void *detptr,
+                                char pol, long lmax, long beammmax,
+                                long order, MPI_Comm comm) {
 
 #ifdef DEBUG
-      std::cout << "conviqt_convolver_convolve called. pnt.size() = " << pntref->size() << std::endl;
-      for ( int i = 0; i<10; ++i ) {
-        std::cout << "pnt[" << i << "] = " << (*pntref)[i] << std::endl;
-      }
+        int size, rank, err;
+
+        err = MPI_Comm_size(comm, &size);
+        if (err) {
+            std::cout << "conviqt_convolver_new: Failed to query communicator size."
+                      << std::endl;
+            return NULL;
+        }
+
+        err = MPI_Comm_rank(comm, &rank);
+        if (err) {
+            std::cout << "conviqt_convolver_new: Failed to query task rank." << std::endl;
+            return NULL;
+        }
+
+        std::cout << "conviqt_convolver_new: Task " << rank << " / " << size
+                  << " initializing." << std::endl;
+
+        std::cout << "conviqt_convolver_new called with pol = " << int(pol)
+                  << ", lmax = " << lmax << ", beammmax = " << beammmax
+                  << ", order = " << order << std::endl;
 #endif
 
-      cnvref->convolve( *pntref, calibrate );
-    } catch (...) {
-      std::cout << "convolve threw an exception:" << std::endl;
-      throw;
-      return -1;
-    }
-    return 0;
-  }
+        if (lmax > LMAXMAX || beammmax > LMAXMAX) {
+            std::cerr << "Suspiciously large convolver parameters: lmax = " << lmax
+                      << ", beammax = " << beammmax << std::endl;
+            return NULL;
+        }
 
+        conviqt::sky *skyref = reinterpret_cast< conviqt::sky * >(skyptr);
+        conviqt::beam *beamref = reinterpret_cast< conviqt::beam * >(beamptr);
+        conviqt::detector *detref = reinterpret_cast< conviqt::detector * >(detptr);
+
+        return new(std::nothrow) conviqt::convolver(skyref, beamref, detref, pol,
+                                                    lmax, beammmax, order, comm);
+    }
+
+    int conviqt_convolver_del(void *ptr) {
+        try {
+            conviqt::convolver *ref = reinterpret_cast< conviqt::convolver * >(ptr);
+            delete ref;
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_convolver_set_sky(void *cnvptr, void *skyptr) {
+        try {
+            conviqt::convolver *cnvref = reinterpret_cast< conviqt::convolver * >(cnvptr);
+            conviqt::sky *skyref = reinterpret_cast< conviqt::sky * >(skyptr);
+            cnvref->set_sky(skyref);
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_convolver_set_beam(void *cnvptr, void *beamptr) {
+        try {
+            conviqt::convolver *cnvref = reinterpret_cast< conviqt::convolver * >(cnvptr);
+            conviqt::beam *beamref = reinterpret_cast< conviqt::beam * >(beamptr);
+            cnvref->set_beam(beamref);
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_convolver_set_detector(void *cnvptr, void *detptr) {
+        try {
+            conviqt::convolver *cnvref = reinterpret_cast< conviqt::convolver * >(cnvptr);
+            conviqt::detector *detref = reinterpret_cast< conviqt::detector * >(detptr);
+            cnvref->set_detector(detref);
+        } catch (...) {
+            return -1;
+        }
+        return 0;
+    }
+
+    int conviqt_convolver_convolve(void *cnvptr, void *pntptr, char calibrate) {
+        try {
+            conviqt::convolver *cnvref = reinterpret_cast< conviqt::convolver * >(cnvptr);
+            conviqt::pointing *pntref = reinterpret_cast< conviqt::pointing * >(pntptr);
+
+#ifdef DEBUG
+            std::cout << "conviqt_convolver_convolve called. pnt.size() = "
+                      << pntref->size() << std::endl;
+            for (int i = 0; i < 10; ++i) {
+                std::cout << "pnt[" << i << "] = " << (*pntref)[i] << std::endl;
+            }
+#endif
+
+            cnvref->convolve(*pntref, calibrate);
+        } catch (...) {
+            std::cout << "convolve threw an exception:" << std::endl;
+            throw;
+            return -1;
+        }
+        return 0;
+    }
 
 }
-
--- src/conviqt_io.cpp
+++ src/conviqt_io.cpp
@@ -20,28 +20,35 @@ int beam::read(long beamlmax, long beammmax, bool beampol,
     }
 
     pol = beampol;
+    int lmax_file, mmax_file;
     if (rank == 0) {
-        int lmax_file = -1;
-        int mmax_file = -1;
-        if (beamlmax < 0 || beammmax < 0) {
-            if (pol) {
-                get_almsize_pol(infile_beam, lmax_file, mmax_file);
-            } else {
+        if (pol) {
+            get_almsize_pol(infile_beam, lmax_file, mmax_file);
+        } else {
             get_almsize(infile_beam, lmax_file, mmax_file);
-            }
-
-        }
-        lmax = beamlmax;
-        mmax = beammmax;
-        if (lmax < 0) {
-            lmax = lmax_file;
-        }
-        if (mmax < 0) {
-            mmax = mmax_file;
         }
     }
-    mpiMgr.bcast(lmax);
-    mpiMgr.bcast(mmax);
+    mpiMgr.bcast(lmax_file);
+    mpiMgr.bcast(mmax_file);
+    // Ensure that the beam in the file has a high enough expansion order.
+    if (lmax_file < beamlmax) {
+        throw std::runtime_error("Beam lmax exceeds available expansion order.");
+    }
+    if (mmax_file < beammmax) {
+        throw std::runtime_error("Beam mmax exceeds available expansion order.");
+    }
+    // lmax
+    if (beamlmax > 0) {
+        lmax = beamlmax;
+    } else {
+        lmax = lmax_file;
+    }
+    // mmax
+    if (beammmax > 0) {
+        mmax = beamlmax;
+    } else {
+        mmax = std::min((int)lmax, mmax_file);
+    }
     fname = infile_beam;
 
     blmT_.Set(lmax, mmax);
@@ -106,37 +113,47 @@ int sky::read(long skylmax, bool skypol,
     }
 
     pol = skypol;
+    int lmax_file, mmax_file;
     if (rank == 0) {
-        if (skylmax < 0) {
-            int lmax_file = -1;
-            int mmax_file = -1;
-            if (pol) {
-                get_almsize_pol(infile_sky, lmax_file, mmax_file);
-            } else {
-                get_almsize(infile_sky, lmax_file, mmax_file);
-            }
-            lmax = lmax_file;
+        if (pol) {
+            get_almsize_pol(infile_sky, lmax_file, mmax_file);
         } else {
-            lmax = skylmax;
+            get_almsize(infile_sky, lmax_file, mmax_file);
         }
     }
-    mpiMgr.bcast(lmax);
+    mpiMgr.bcast(lmax_file);
+    mpiMgr.bcast(mmax_file);
+    // If user provided skylmax, ensure that the file has high enough
+    // expansion order
+    if (lmax_file < skylmax) {
+        throw std::runtime_error("Sky lmax exceeds available expansion order.");
+    }
+    if (skylmax > 0) {
+        lmax = skylmax;
+    } else {
+        lmax = lmax_file;
+    }
+    // We require equal lmax and mmax
+    if (mmax_file < lmax) {
+        throw std::runtime_error("Sky mmax exceeds available expansion order.");
+    }
+    int mmax = lmax;
     fname = infile_sky;
     fwhm_deconv = fwhm_deconv_sky;
 
     slmT_.Set(lmax, lmax);
     if (pol) {
-        slmG_.Set(lmax, lmax);
-        slmC_.Set(lmax, lmax);
+        slmG_.Set(lmax, mmax);
+        slmC_.Set(lmax, mmax);
     }
 
     long slmsize = (lmax + 1) * (lmax + 2);
 
     if (rank == 0) {
         if (pol) {
-            read_Alm_from_fits(infile_sky, slmT_, slmG_, slmC_, lmax, lmax);
+            read_Alm_from_fits(infile_sky, slmT_, slmG_, slmC_, lmax, mmax);
         } else {
-            read_Alm_from_fits (infile_sky, slmT_, lmax, lmax);
+            read_Alm_from_fits (infile_sky, slmT_, lmax, mmax);
         }
         if (CMULT_VERBOSITY > 1) {
             std::cout << "Done reading sky alms on task = " << rank << std::endl;
