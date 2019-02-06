69172bffe7db9182da431cd2f3f46f8af4adeb4b
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2019-02-05 16:28:48 -0800

    Removed old source and Makefiles

--- old-src/levels_facilities.h
+++
@@ -1,7 +0,0 @@
-#ifndef LEVELS_FACILITIES_H
-#define LEVELS_FACILITIES_H
-
-int cmult_module (int argc, const char **argv);
-int LevelS_module (const char *module, int argc, const char **argv);
-
-#endif
