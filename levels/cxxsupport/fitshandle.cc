edf0c302f4b1d2483f9074511b095d9d5e3808bb
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2022-03-23 12:28:12 -0700

    Fix FITS version check. The version fields are two, not three digits wide

--- levels/cxxsupport/fitshandle.cc
+++ levels/cxxsupport/fitshandle.cc
@@ -805,26 +805,26 @@ class cfitsio_checker
       levels_assert(fits_get_version(&fitsversion),
         "error calling fits_get_version()");
       /* See https://sourceforge.net/p/healpix/bugs/122/
-       * CFITSIO 4.x switched to a three version format (4.0.0), as opposed
+       * CFITSIO 4.x switched to a three version format (4.00.00), as opposed
        * to previous two-number versions (3.47). Version 4 defines a new macro
        * CFITSIO_MICRO to track the patch level in the version. We check if
        * macro is defined, and fall back to the old behaviour if it is not.
        * If it is, we adapt to the new value returned by 'fits_get_version'.
        */
 #ifdef CFITSIO_MICRO
-      int v_header  = nearest<int>(1E6*CFITSIO_MAJOR + 1000.*CFITSIO_MINOR + CFITSIO_MICRO),
-          v_library = nearest<int>(1E6*fitsversion);
+      int v_header  = nearest<int>(10000*CFITSIO_MAJOR + 100*CFITSIO_MINOR + CFITSIO_MICRO),
+          v_library = nearest<int>(10000*fitsversion);
       if (v_header!=v_library)
           cerr << endl << "WARNING: version mismatch between CFITSIO header (v"
-               << dataToString(v_header*1.0E-6) << ") and linked library (v"
-               << dataToString(v_library*1.0E-6) << ")." << endl << endl;
+               << dataToString(v_header*1.0E-4) << ") and linked library (v"
+               << dataToString(v_library*1.0E-4) << ")." << endl << endl;
 #else
-      int v_header  = nearest<int>(1000.*CFITSIO_VERSION),
-          v_library = nearest<int>(1000.*fitsversion);
+      int v_header  = nearest<int>(100*CFITSIO_VERSION),
+          v_library = nearest<int>(100*fitsversion);
       if (v_header!=v_library)
           cerr << endl << "WARNING: version mismatch between CFITSIO header (v"
-               << dataToString(v_header*0.001) << ") and linked library (v"
-               << dataToString(v_library*0.001) << ")." << endl << endl;
+               << dataToString(v_header*0.01) << ") and linked library (v"
+               << dataToString(v_library*0.01) << ")." << endl << endl;
 #endif
       }
   };
